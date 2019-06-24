f7117d51172218850bb40dc2571ae36e968a9902
Joohyun Park <jhp527@nyu.edu>
2019-06-23 20:58:15 -0400

    update chart on resize

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let habitable = [];
+
 let margin = {
         top: 20,
-        right: 40,
+        right: 100,
         bottom: 20,
-        left: 40
+        left: 100
     },
     width = document.querySelector(".container").getBoundingClientRect().width - margin.left - margin.right,
     height = window.innerHeight * 0.65 - margin.top - margin.bottom;
@@ -21,8 +23,10 @@ const init = (data) => {
             return d.st_dist;
         }))
         .range([0, width]);
+
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
+        .attr("class", "axis")
         .call(d3.axisBottom(x));
 
     // Add a scale for bubble size
@@ -38,6 +42,7 @@ const init = (data) => {
         .data(data)
         .enter()
         .append("circle")
+        .attr("class", "planets")
         .attr("cx", function (d) {
             return x(d.st_dist);
         })
@@ -62,69 +67,12 @@ const init = (data) => {
 }
 
 
-const list = [
-"Teegarden b",
-"K2-72 e",
-"GJ 3323 b",
-"TRAPPIST-1 d",
-"TRAPPIST-1 e",
-"GJ 667 C f",
-"Proxima Cen b",
-"Kepler-442 b",
-"GJ 273 b",
-"Wolf 1061 c",
-"GJ 667 C c",
-"tau Cet e",
-"Kepler-1229 b",
-"GJ 667 C e",
-"TRAPPIST-1 f",
-"Kepler-62 f",
-"Teegarden c",
-"TRAPPIST-1 g",
-"Kepler-186 f",
-"Kepler-452 b",
-"Kepler-62 e",
-"Kepler-1652 b",
-"Kepler-1544 b",
-"K2-3 d",
-"Kepler-296 e",
-"Kepler-283 c",
-"Kepler-1410 b",
-"Kepler-1638 b",
-"K2-296 b",
-"Kepler-296 f",
-"Kepler-705 b",
-"Kepler-440 b",
-"Kepler-1653 b",
-"GJ 832 c",
-"Kepler-1606 b",
-"Kepler-1090 b",
-"Kepler-61 b",
-"Kepler-443 b",
-"K2-18 b",
-"Kepler-22 b",
-"K2-9 b",
-"Kepler-26 e",
-"Kepler-1552 b",
-"Kepler-1540 b",
-"LHS 1140 b",
-"Kepler-1632 b",
-"HD 40307 g",
-"GJ 163 c",
-"Kepler-298 d",
-"K2-288 B b",
-"GJ 3293 d",
-"Kepler-174 d"
-];
-
-
 fetch('https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&format=json')
     .then(response => {
         return response.json();
     })
     .then(data => {
         //        console.log(data);
-        let habitable = [];
         data.forEach((val) => {
             list.forEach(el => {
                 if (val.pl_name == el) {
@@ -132,7 +80,7 @@ fetch('https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?t
                 }
             });
         });
-        console.log(habitable);
+//        console.log(habitable);
         init(habitable);
     })
     .catch(err => {
@@ -140,5 +88,29 @@ fetch('https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?t
     });
 
 
+const resize = () => {
+    width = document.querySelector(".container").getBoundingClientRect().width - margin.left - margin.right;
+    height = window.innerHeight * 0.65 - margin.top - margin.bottom;
 
- window.addEventListener("resize", ()=>{console.log('resized')});
+    x = d3.scaleLinear()
+        .domain(d3.extent(habitable, function (d) {
+            return d.st_dist;
+        }))
+        .range([0, width]);
+
+    d3.selectAll(".axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    d3.selectAll(".planets")
+        .attr("cx", function (d) {
+            return x(d.st_dist);
+        })
+        .attr("cy", window.innerHeight / 2);
+
+    d3.select(".earth")
+        .attr("cx", 0)
+        .attr("cy", window.innerHeight / 2);
+}
+
+window.addEventListener("resize", resize);
